0a8e565163205c21d06e39d257fcb6a87ca33a77
joe <joe@joes-MacBook-Pro.local>
2018-05-11 20:20:32 +0100

    added add amd multiply methods and tested both

--- SimpleNumber.rb
+++ SimpleNumber.rb
@@ -2,9 +2,17 @@
 
 class SimpleNumber
 
-  def inititialize(num)
+  def initialize(num)
     raise unless num.is_a?(Numeric)
     @x = num
+  end
+
+  def add(y)
+    @x + y
+  end
+
+  def multiply(y)
+    @x * y
   end
 
 end
--- TestSimpleNumber.rb
+++ TestSimpleNumber.rb
@@ -5,8 +5,8 @@ require 'test/unit'
 class TestSimpleNumber < Test::Unit::TestCase
 
   def test_simple
-    assertEqual(4, SimpleNumber.new(2).add(2))
-    assertEqual(6, SimpleNumber.new(2).multiply(3))
+    assert_equal(4, SimpleNumber.new(2).add(2))
+    assert_equal(6, SimpleNumber.new(2).multiply(3))
   end
 
 end
